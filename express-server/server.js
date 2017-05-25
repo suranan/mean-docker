ec34a01f2b1f6f21bf0222360b50411a07df5d48
Suranan Amnucgmanee <amnucgmanee@gmail.com>
2017-05-25 12:26:47 +0700

    add access allow origin

--- express-server/server.js
+++ express-server/server.js
@@ -9,12 +9,23 @@ const api = require('./routes/api');
 
 const app = express();
 
+// Code commented out for brevity
+
 // Parsers for POST data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Cross Origin middleware
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    next()
+})
+
 // Set our api routes
 app.use('/', api);
+
+// Code commented out for brevity
 
 /**
  * Get port from environment and store in Express.
